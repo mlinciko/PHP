148f61a61ff7c0e526c7cebe4828d7f9ed86e294
Ovcharenko Maria <mlinciko@192.168.0.16>
2022-12-18 10:54:54 +0300

    Практика 6 fixes

--- src/static/controllers/createCharts.php
+++ src/static/controllers/createCharts.php
@@ -6,15 +6,15 @@ use Amenadiel\JpGraph\Plot;
 
 
 //Круговая диаграмма
-$slicesIndicators1 = array_slice($fakeIndicatorData, 40);
-$slicesCountries1 = array_slice($fakeCountryData, 40);
+$slicesIndicators = array_slice($fakeIndicatorData, 40);
+$slicesCountries = array_slice($fakeCountryData, 40);
 
 $graph1 = new Graph\PieGraph(700, 400);
 $graph1->title->Set("A Simple Pie Plot");
 $graph1->SetBox(true);
 
-$plot1 = new Plot\PiePlot($slicesIndicators1);
-$plot1->SetLegends($slicesCountries1);
+$plot1 = new Plot\PiePlot($slicesIndicators);
+$plot1->SetLegends($slicesCountries);
 $plot1->ShowBorder();
 $plot1->SetColor('black');
 $plot1->SetSliceColors(array('#9F0142', '#D63E50', '#F36D43', '#FDAD61', '#FEE08B', '#E6F698', '#ABDEA4', '#66C2A4', '#3388BD', '#5E50A2'));
@@ -24,8 +24,8 @@ $graph1->Stroke("chart1.png");
 
 
 //Столбчатая диаграмма
-$slicesIndicators2 = array_slice($fakeIndicatorData, 0, 10);
-$slicesCountries2 = array_slice($fakeCountryData, 0, 10);
+$slicesAges = array_slice($fakeAgeData, 0, 10);
+$slicesNames = array_slice($fakeNameData, 0, 10);
 
 $graph2 = new Graph\Graph(1100,400);
 $graph2->SetScale("textlin");
@@ -34,18 +34,18 @@ $graph2->title->Set("Bar Plots");
 
 $graph2->xaxis->title->Set("Country");
 $graph2->xaxis->title->SetMargin(5);
-$graph2->xaxis->SetTickLabels($slicesCountries2);
+$graph2->xaxis->SetTickLabels($slicesNames);
 $graph2->yaxis->title->Set("Indicator");
 
-$plot2 = new Plot\BarPlot($slicesIndicators2);
+$plot2 = new Plot\BarPlot($slicesAges);
 $plot2->SetFillColor('#5E50A2');
 
 $graph2->Add($plot2);
 $graph2->Stroke("chart2.png");
 
 //Линейная диаграмма
-$slicesIndicators3 = array_slice($fakeIndicatorData, 10, 10);
-$slicesCountries3 = array_slice($fakeCountryData, 10, 10);
+$slicesIndicators2 = array_slice($fakeIndicatorData, 10, 10);
+$slicesCountries2 = array_slice($fakeCountryData, 10, 10);
 
 $graph3 = new Graph\Graph(1100,400);
 $graph3->SetScale("textlin");
@@ -64,11 +64,11 @@ $graph3->yaxis->HideTicks(false,false);
 
 $graph3->xgrid->Show();
 $graph3->xgrid->SetLineStyle("solid");
-$graph3->xaxis->SetTickLabels($slicesCountries3);
+$graph3->xaxis->SetTickLabels($slicesCountries2);
 $graph3->xgrid->SetColor('#E3E3E3');
 
 // Create line
-$plot3 = new Plot\LinePlot($slicesIndicators3);
+$plot3 = new Plot\LinePlot($slicesIndicators2);
 $plot3->SetColor("#6495ED");
 $plot3->SetLegend('Country');
 
--- src/static/controllers/dataGenerator.php
+++ src/static/controllers/dataGenerator.php
@@ -4,8 +4,14 @@ $faker = Faker\Factory::create();
 
 $fakeIndicatorData = array();
 $fakeCountryData = array();
+$fakeNameData = array();
+$fakeAgeData = array();
+$fakeColorData = array();
 for($i = 0; $i < 50; ++$i) {
     $fakeIndicatorData[$i] = $faker->randomDigitNotNull();
     $fakeCountryData[$i] = $faker->country();
+    $fakeNameData[$i] = $faker->name();
+    $fakeAgeData[$i] = $faker->numberBetween(0, 100);
+    $fakeColorData[$i] = $faker->colorName();
 }
 ?>
